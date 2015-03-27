7e82cdd4d92155bd23b5f28613232b5b44161ef9
Dan Nemeth <dnemeth@pivotal.io>
2015-03-27 10:56:41 -0400

    [#89223826] Added more meaningful response testing

--- server_test.go
+++ server_test.go
@@ -68,13 +68,19 @@ var _ = Describe("Server", func() {
 
 			It("returns a search result", func() {
 				server.ServeHTTP(recorder, request)
-				Ω(recorder.Body).To(ContainSubstring("abc"))
-				Ω(recorder.Body).To(ContainSubstring("_meta"))
+				mJSON := mapFromJSON(recorder.Body.Bytes())
+				Ω(mJSON["status"]).To(Equal("success"))
+				results := sliceFromInterface(mJSON["data"])
+				first := results[0].(map[string]interface{})
+				last := results[11].(map[string]interface{})
+				Ω(len(results)).To(Equal(12))
+				Ω(first["_type"]).To(Equal(TypeResource))
+				Ω(last["_type"]).To(Equal(ItemResource))
 			})
 		})
 	})
 
-	Describe("GET /v1/search with scope keyword", func() {
+	Describe("GET /v1/search with item scope", func() {
 		BeforeEach(func() {
 			request, _ = http.NewRequest("GET", "/v1/search?q=abc scope:item", nil)
 		})
@@ -87,8 +93,39 @@ var _ = Describe("Server", func() {
 
 			It("returns a search result", func() {
 				server.ServeHTTP(recorder, request)
-				Ω(recorder.Body).To(ContainSubstring("abc"))
-				Ω(recorder.Body).To(ContainSubstring("_meta"))
+				mJSON := mapFromJSON(recorder.Body.Bytes())
+				Ω(mJSON["status"]).To(Equal("success"))
+				results := sliceFromInterface(mJSON["data"])
+				first := results[0].(map[string]interface{})
+				last := results[9].(map[string]interface{})
+				Ω(len(results)).To(Equal(10))
+				Ω(first["_type"]).To(Equal(ItemResource))
+				Ω(last["_type"]).To(Equal(ItemResource))
+			})
+		})
+	})
+
+	Describe("GET /v1/search with type scope", func() {
+		BeforeEach(func() {
+			request, _ = http.NewRequest("GET", "/v1/search?q=abc scope:type", nil)
+		})
+
+		Context("global search with results", func() {
+			It("returns a status code of 200", func() {
+				server.ServeHTTP(recorder, request)
+				Ω(recorder.Code).To(Equal(200))
+			})
+
+			It("returns a search result", func() {
+				server.ServeHTTP(recorder, request)
+				mJSON := mapFromJSON(recorder.Body.Bytes())
+				Ω(mJSON["status"]).To(Equal("success"))
+				results := sliceFromInterface(mJSON["data"])
+				first := results[0].(map[string]interface{})
+				last := results[1].(map[string]interface{})
+				Ω(len(results)).To(Equal(2))
+				Ω(first["_type"]).To(Equal(TypeResource))
+				Ω(last["_type"]).To(Equal(TypeResource))
 			})
 		})
 	})
